8fc66e3de37181c383a97deaf2fc47dbda107669
radityawildanassyaif <radityawildan33@gmail.com>
2026-05-23 18:56:54 +0700

    update dashboard

--- src/pages/dashboard/event/EventEdit.tsx
+++ src/pages/dashboard/event/EventEdit.tsx
@@ -5,7 +5,7 @@ import { useNavigate, useParams, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "https://backend-invofest-alpha.vercel.app";
+const API_URL = import.meta.env.VITE_API_URL || "https://backend-infovest-f8hl.vercel.app/speakers";
 
 const schema = z.object({
   name: z.string().min(3, "Nama event minimal 3 karakter"),
--- src/pages/dashboard/event/EventIndex.tsx
+++ src/pages/dashboard/event/EventIndex.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 
 // ===== SERVICE =====
 // PERBAIKAN: Mengubah URL lokal ke URL Backend Vercel yang sudah live
-const BASE_URL = "https://backend-invofest-six.vercel.app/events";
+const BASE_URL = "https://backend-infovest-f8hl.vercel.app/speakers";
 
 // PERBAIKAN 1: Sesuaikan tipe data dengan skema asli Prisma & Controller Backend
 type Event = {
